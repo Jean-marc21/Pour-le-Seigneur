31110d7f18d8ab750b0ff4889f4d99bfb4197893
jean-marc21 <marcodechristjeanyayaka@gmail.com>
2021-10-09 10:29:48 +0000

    Amélioration de l'interface de Home.js

--- Components/Home.js
+++ Components/Home.js
@@ -2,23 +2,54 @@
 // Page d'accueil de l'application
 
 import React from 'react'
-import {Text, Box, VStack, HStack, InputGroup, Input, Icon, IconButton, FlatList} from 'native-base';
-import {MaterialCommunityIcons} from '@expo/vector-icons';
+import {
+    Text, Box, VStack, HStack, InputGroup, Input, Icon, IconButton, FlatList, Pressable, Center,
+    Heading
+} from 'native-base';
+import {MaterialIcons} from '@expo/vector-icons';
+
+import songs from '../DataBase/songs';
+import {gridAutoColumns} from 'styled-system';
+
+const data = songs[0].songs;
+
+function Song ({item}){
+    return (
+        <Pressable
+            onPress={()=>{console.log("Hello word !")}}
+        >
+            <Box>
+                <HStack>
+                    <Box flex={1}>
+                        <Heading size='md'>{item.n}</Heading>
+                    </Box>
+                    <Box flex={5}>
+                       <VStack>
+                            <Heading size='sm'>{item.title}</Heading>
+                            <Text fontFamily='fantasy' fontStyle='italic'>{item.author}</Text>  
+                        </VStack> 
+                    </Box>
+                </HStack>
+            </Box>
+        </Pressable>
+    )
+}
 
 function Home (props){
     return (
         <Box safeArea>
             <VStack>
-                <HStack>
-                    <InputGroup>
-                        <Input />
-                        <IconButton
-                            icon={<Icon as={<MaterialCommunityIcons name="research"/>}/>}/>
-                    </InputGroup>
+                <HStack mb={3} pb={2} shadow={5}>
+                    <Input placeholder='title'
+                        mx={2}
+                        isFullWidth={true}
+                        InputRightElement={<Icon as={<MaterialIcons name='search'/>}/>}/>
                 </HStack>
-
-                <HStack>
-                    <Text>Les chansons</Text>
+                
+                <HStack marginTop='md'>
+                    <FlatList 
+                        data={data}
+                        renderItem={Song}/>
                 </HStack>
             </VStack>
         </Box>
---
+++ DataBase/songs.js
@@ -0,0 +1,45 @@
+const songs = [
+    // On cree des recueils de cantiques
+   {
+        name : 'Christ est vainqueur',
+        version : '1.0',
+        modifiedOn : '08-10-2021',
+        lang : 'fr',
+        songs : [
+            {
+                id : '1',
+                n : 1,
+                receuil : '',
+                ref : '',
+                title :'Grand Dieu, nous te bénissons',
+                author : 'EMPEYTAZ',
+                lyrics : [
+                    "Grand Dieu, nous te bénissons,\nNous célébrons tes louanges ;\nEternel, nous t'exaltons\nDe concert avec les anges,"
+                    +"\n{ Et prosternés devant toi,\nNous t'adorons,ô grand Roi } (bis)",
+                    "Puisse ton règne de paix\nS'étendre par tout le monde ;\nDès maintenant à jamais,Que sur la terre et sur l'onde,"
+                    +"\n{ Tous genoux soient abattus\nAu nom du Seigneur Jésus } (bis)",
+                    "Gloire soit au Saint-Esprit !\nGloire soit à Dieu le Père !\nGloire soit à Jésus-Christ,\nNotre Sauveur, notre frère ;"
+                    +"\n{ Son immense charité\nDure à perpétuité } (bis)"
+                ]
+            },
+            {
+                id : '1200',
+                n : 354,
+                receuil : '',
+                ref : '',
+                title :'Testons la chanson imaginaire',
+                author : 'Author',
+                lyrics : [
+                    "Grand Dieu, nous te bénissons,\nNous célébrons tes louanges ;\nEternel, nous t'exaltons\nDe concert avec les anges,"
+                    +"\n{ Et prosternés devant toi,\nNous t'adorons,ô grand Roi } (bis)",
+                    "Puisse ton règne de paix\nS'étendre par tout le monde ;\nDès maintenant à jamais,Que sur la terre et sur l'onde,"
+                    +"\n{ Tous genoux soient abattus\nAu nom du Seigneur Jésus } (bis)",
+                    "Gloire soit au Saint-Esprit !\nGloire soit à Dieu le Père !\nGloire soit à Jésus-Christ,\nNotre Sauveur, notre frère ;"
+                    +"\n{ Son immense charité\nDure à perpétuité } (bis)"
+                ]
+            }
+        ]
+    }
+]
+
+export default songs;
